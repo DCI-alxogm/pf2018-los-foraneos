06e5e7e4adbc702b14f15f8ce8cd4a5fbc6b54b0
VDMM858 <42146347+VDMM858@users.noreply.github.com>
2018-12-06 21:21:41 -0600

    Add files via upload

--- principal.c
+++ principal.c
@@ -1,22 +1,34 @@
-//programa principal que le pide al usuario que ingrese uno de los dos estimadores 
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
+
 int main(){
-	int o;
-	float Esi,ELS;
-	printf("Elija el estimador\n");
-	printf("(1)Estimador simple\n (2)Estimador Landy-Szalay");
-	scanf("%i",&o);
+	int op=1,o,nR,nD,DD,RR,nDR,DR;
+	float Es, Els;
+	
+while(op==1){
+	printf("Qué estimador desea usar?\n");
+	printf("Presione (1) para el estimador simple\n");
+	printf("Presione (2) para el estimador Landy-Szalay\n");
+
+	scanf("%i", &o);
+	
 	switch(o){
-		case 1: 
-			Esi=((nr/nd)(DD/RR))-1; //simple
+		case 1:
+			printf("Seleccionaste el estimador simple\n");
+			
+			 Es=((nR/nD)*(DD/RR))-1;
+			 
+			 break;
+			
+		case 2:
+			printf("Seleccionaste el estimador Landy-Szalay\n");
+			
+			Els=((DD/nD)-(2*DR/nDR)+(RR/nR))/(RR/nR);
+			
 			break;
-		case 2: 
-			ELS=((DD/nd)-(2*DR/Ndr)+(RR/nr)/(RR/nr));
-			break;
-		
-		}
-	return 0;
+	}
 }
 
+	return 0;
+}
