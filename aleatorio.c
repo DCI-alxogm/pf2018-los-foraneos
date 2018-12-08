40746ed4a8e84e7a27ce7da06225378ede32703c
antoniosanch <42146544+antoniosanch@users.noreply.github.com>
2018-12-08 03:07:24 -0600

    Add files via upload

--- aleatorio.c
+++ aleatorio.c
@@ -7,8 +7,8 @@ int rdtsc(){
 		__asm__ __volatile__("rdtsc");
 	}
 int main(){
-funcion_ale();
-funDD();
+	funcion_ale();
+	funDD();
 return 0;		
 }
 
@@ -21,10 +21,10 @@ void funDD(){
 	srand(rdtsc());
 	for(i=0;i<=9;i++){
 	srand(rdtsc());
-		divi=1+rand()%30;
-		xt=(25+rand()%30)/divi;
-		yt=(25+rand()%30)/divi;
-		zt=(25+rand()%20)/divi;
+		divi=1+rand()%3;
+		xt=(25+rand()%1000)/divi;
+		yt=(25+rand()%1000)/divi;
+		zt=(25+rand()%1000)/divi;
 		
 		fprintf(fp,"%f\t",xl[i]=xt);
 		fprintf(fp,"%f\t",yl[i]=yt);
@@ -34,34 +34,32 @@ void funDD(){
 	fclose(fp);
 }
 	
-	
-
 
 
 void funcion_ale(){
 	FILE *fp;
-	fp=fopen("random.txt","w");
+	fp=fopen("RR.txt","w");
 	int i;
-	float var,var2,yva,zva;
-	float x[9],y[9],z[9];
-	srand(time(NULL));
+	float xt,divi,yt,zt;
+	float xl[9],yl[9],zl[9];
+	srand(rdtsc());
 	for(i=0;i<=9;i++){
-		do{
-		var2=rand();
-		}while(var2>0&&var2<15);
-		var=(1+rand()*((10+1)))/var2;
-		yva=(1+rand()*((10+1)))/var2;
-		zva=(1+rand()*((10+1)))/var2;
-		fprintf(fp,"%f\t",x[i]=var);
-		fprintf(fp,"%f\t",y[i]=yva);
-		fprintf(fp,"%f\n",z[i]=zva);
+	srand(rdtsc());
+		divi=1+rand()%30;
+		xt=(25+rand()%40)/divi;
+		yt=(25+rand()%40)/divi;
+		zt=(25+rand()%20)/divi;
+		
+		fprintf(fp,"%f\t",xl[i]=xt);
+		fprintf(fp,"%f\t",yl[i]=yt);
+		fprintf(fp,"%f\n",zl[i]=zt);
 
 	}
 	fclose(fp);
 }
 
 
-//	float num,i;
+//	float num,
 //	srand(time(NULL));
 //	for(i=0.0;i<=29;i++){
 //		num=1+rand()%((10+1));
